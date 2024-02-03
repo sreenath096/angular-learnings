784400d42940229b97e162e046ada7e225b40275
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-03 22:00:01 +0530

    Different types of Selectors supported for creating components

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-users',
+  //selector: 'app-users',
+  //selector: '[app-users]',
+  selector: '.app-users',
   template: `<h3>Users Component</h3> <app-user></app-user>
   <app-user></app-user>`,
   styles: 'h3 { color: blue}'
