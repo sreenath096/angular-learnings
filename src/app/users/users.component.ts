ff6250a9c7c2581109f0ea6671d29ee513571daf
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-03 21:42:56 +0530

    Template and TemplateUrl in the angular components

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
-  templateUrl: './users.component.html',
+  template: `<app-user></app-user>
+  <app-user></app-user>`,
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
