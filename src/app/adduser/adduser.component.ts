5f4560c00f3d98cab0e28a5c6256a36f41c95866
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-05 20:21:25 +0530

    Injecting services into components using Dependency Injection through Providers

--- src/app/adduser/adduser.component.ts
+++ src/app/adduser/adduser.component.ts
@@ -4,17 +4,21 @@ import { LoggingService } from '../services/logging.service';
 @Component({
   selector: 'app-adduser',
   templateUrl: './adduser.component.html',
-  styleUrl: './adduser.component.css'
+  styleUrl: './adduser.component.css',
+  providers: [LoggingService]
 })
 export class AdduserComponent {
   userName: string = '';
   @Output() userAdded = new EventEmitter<string>();
   @ViewChild('userInput') userInput: ElementRef = {} as ElementRef;
 
+  constructor(private loggingService: LoggingService) {
+
+  }
+
   onUserAdded() {
     this.userName = this.userInput.nativeElement.value;
     this.userAdded.emit(this.userInput.nativeElement.value);
-    let loggingService = new LoggingService();
-    loggingService.logToConsole("User is added " + this.userName);
+    this.loggingService.logToConsole("User is added " + this.userName);
   }
 }
