bc5dc36d449521b3e32263bf3d1b1a3f822cda88
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-05 17:25:24 +0530

    Using HostBinding Decorator to bind the Properties for the Directive element

--- src/app/Directives/renderer-highlight.directive.ts
+++ src/app/Directives/renderer-highlight.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRendererHighlight]'
 })
 export class RendererHighlightDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') color: string | undefined;
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
@@ -12,14 +13,14 @@ export class RendererHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') onmouseover(event: Event) {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'red');
+    this.color = 'red';
   }
 
   @HostListener('mouseleave') onmouseleave(event: Event) {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'yellow');
+    this.color = 'yellow';
   }
 
   @HostListener('click') onclick(event: Event) {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'green');
+    this.color = 'pink';
   }
 }
