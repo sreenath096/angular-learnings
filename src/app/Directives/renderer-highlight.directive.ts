707a9ad78614769c60ec948d2d6e6d81597829d4
Sreenath Angamuthu <sreenath096@gmail.com>
2024-02-05 17:37:36 +0530

    Sending input data to the Directives as Input properties

--- src/app/Directives/renderer-highlight.directive.ts
+++ src/app/Directives/renderer-highlight.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRendererHighlight]'
 })
 export class RendererHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'red';
+  @Input() highlightColor: string = 'yellow';
 
   @HostBinding('style.backgroundColor') color: string | undefined;
   constructor(private element: ElementRef, private renderer: Renderer2) { }
@@ -13,11 +15,11 @@ export class RendererHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') onmouseover(event: Event) {
-    this.color = 'red';
+    this.color = this.highlightColor;
   }
 
   @HostListener('mouseleave') onmouseleave(event: Event) {
-    this.color = 'yellow';
+    this.color = this.defaultColor;
   }
 
   @HostListener('click') onclick(event: Event) {
